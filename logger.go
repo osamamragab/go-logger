70e46c3d41c2f3b5f047fefa5674c20ea40986ca
Osama <iosamaify@gmail.com>
2020-08-01 08:26:37 +0200

    add: SetColors func

--- logger.go
+++ logger.go
@@ -1,34 +1,70 @@
 package logger
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-var colors = map[string]string{
-	"green":  "\x1b[32m",
-	"red":    "\x1b[31m",
-	"yellow": "\x1b[33m",
-	"blue":   "\x1b[34m",
+// Colors represents ASCII colors for console
+type Colors struct {
+	Success, Error, Warn, Info string
 }
 
-func format(msg, color string) string {
-	return color + "[" + msg + "]" + "\x1b[0m" + ":"
+var (
+	colors = Colors{
+		Success: "\x1b[32m",
+		Error:   "\x1b[31m",
+		Warn:    "\x1b[33m",
+		Info:    "\x1b[34m",
+	}
+
+	colorReset = "\x1b[0m"
+)
+
+func formatWithColor(text, color string) string {
+	return color + "[" + text + "]" + colorReset + ":"
+}
+
+// SetColors override defualt ASCII colors for conosle
+func SetColors(c *Colors) {
+	c.Success = strings.TrimSpace(c.Success)
+	c.Error = strings.TrimSpace(c.Error)
+	c.Warn = strings.TrimSpace(c.Warn)
+	c.Info = strings.TrimSpace(c.Info)
+
+	if c.Success != "" {
+		colors.Success = c.Success
+	}
+
+	if c.Error != "" {
+		colors.Error = c.Error
+	}
+
+	if c.Warn != "" {
+		colors.Warn = c.Warn
+	}
+
+	if c.Info != "" {
+		colors.Info = c.Info
+	}
 }
 
 // Success prints a success message
 func Success(msg string, a ...interface{}) {
-	fmt.Println(format("SUCCESS", colors["green"]), fmt.Sprintf(msg, a...))
+	fmt.Println(formatWithColor("SUCCESS", colors.Success), fmt.Sprintf(msg, a...))
 }
 
 // Error prints a error message
 func Error(msg string, a ...interface{}) {
-	fmt.Println(format("ERROR", colors["red"]), fmt.Sprintf(msg, a...))
+	fmt.Println(formatWithColor("ERROR", colors.Error), fmt.Sprintf(msg, a...))
 }
 
 // Warn prints a warning message
 func Warn(msg string, a ...interface{}) {
-	fmt.Println(format("WARNING", colors["yellow"]), fmt.Sprintf(msg, a...))
+	fmt.Println(formatWithColor("WARNING", colors.Warn), fmt.Sprintf(msg, a...))
 }
 
 // Info prints an info message
 func Info(msg string, a ...interface{}) {
-	fmt.Println(format("INFO", colors["blue"]), fmt.Sprintf(msg, a...))
+	fmt.Println(formatWithColor("INFO", colors.Info), fmt.Sprintf(msg, a...))
 }
