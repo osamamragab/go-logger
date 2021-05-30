934ea060c265ec1a476c361d260d68ed9201355f
Osama <iosamaify@gmail.com>
2021-05-30 07:06:45 +0200

    add Formatter type

--- format.go
+++ format.go
@@ -10,6 +10,10 @@ const (
 	timeFormat string = "2006-01-02 15:04:05"
 )
 
+// Formatter is the formatting function used to format log messages.
+type Formatter func(colors Colors, label, msg string, a ...interface{}) string
+
+// DefaultFormatter is the default formatting function, implements the Formatter type.
 func DefaultFormatter(colors Colors, label, msg string, a ...interface{}) string {
 	if label == "" {
 		return fmt.Sprintf("%s[%s]%s: %s", timeColor, time.Now().Format(timeFormat), colorReset, fmt.Sprintf(msg, a...))
