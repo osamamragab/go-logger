bf78bab9816e393f96c67ed0396080cb1696bb70
Osama <iosamaify@gmail.com>
2021-05-30 07:01:41 +0200

    add Colors type, DefaultColors var

---
+++ colors.go
@@ -0,0 +1,21 @@
+package logger
+
+const colorReset string = "\x1b[0m"
+
+// Colors describes console ASCII colors.
+type Colors map[string]string
+
+// Get returns the value to the given key.
+func (c Colors) Get(key string) string {
+	if c == nil {
+		return ""
+	}
+	return c[key]
+}
+
+var DefaultColors = Colors{
+	"SUCCESS": "\x1b[32m",
+	"ERROR":   "\x1b[31m",
+	"WARN":    "\x1b[33m",
+	"INFO":    "\x1b[34m",
+}
